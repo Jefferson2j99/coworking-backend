06631dc4cfec289cfbf8da9980faed6df82cd525
Matheus Antonino <matrajano12@gmail.com>
2020-03-27 21:43:37 -0300

    Add port

--- src/index.js
+++ src/index.js
@@ -17,4 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+const port = process.env.PORT || 3333;
+server.listen(port, () => {
+  console.log(`Projeto rodando na porta ${port}`);
+});
