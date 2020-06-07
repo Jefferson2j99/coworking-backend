4dc62ccada0ffd2cdb7f47756422ee06beda3b34
Matheus Antonino <matrajano12@gmail.com>
2020-06-06 22:07:17 -0300

    Update server for suported socket io

--- src/index.js
+++ src/index.js
@@ -2,15 +2,34 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const socketIo = require("socket.io");
+const http = require("http");
 
 const routes = require("./routes");
 
 const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
 
 mongoose.connect(process.env.URL_DB, {
   useCreateIndex: true,
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+});
+
+const connectedUsers = {};
+
+io.on("connection", (socket) => {
+  const { company_id: companyId } = socket.handshake.query;
+
+  connectedUsers[companyId] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
 });
 
 app.use(cors());
@@ -18,6 +37,6 @@ app.use(express.json());
 app.use(routes);
 
 const port = process.env.PORT || 3333;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Projeto rodando na porta ${port}`);
 });
